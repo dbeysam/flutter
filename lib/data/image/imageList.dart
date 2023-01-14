5e7f7ee21972b8b9c5d2a72e34ddf3e71e82e7be
dbeysam~ <bayupurbayanto@gmail.com>
2023-01-14 09:22:13 +0700

    instagram

---
+++ lib/data/image/imageList.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+// import '../../data/image/image.dart';
+
+// class Image {
+//   String? id;
+//   bool? LikedByUser;
+//   String? description;
+//   User? user;
+//   Urls? urls;
+
+//   Image{{
+//     this.id,
+//     this.LikedByUser,
+//     this.description,
+//     this.user,
+//     this.urls,
+//   }};
+
+//   Image.fromJson(Map<String, dynamic> user = jsonDecode(jsonString);
+//   print)
+// }
